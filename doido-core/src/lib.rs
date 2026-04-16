b3df5a7dfbaa94100af6905746b6514052d22a1f
Fabiano Martins <fabiano.paula.martins@gmail.com>
2026-04-16 09:48:06 -0300

    feat(core): finalize public API and re-exports

--- doido-core/src/lib.rs
+++ doido-core/src/lib.rs
@@ -2,6 +2,12 @@ pub mod error;
 pub mod trace;
 pub mod inflector;
 
+// Convenience re-exports so downstream crates depend only on doido-core.
 pub use ::anyhow;
 pub use ::thiserror;
+pub use ::async_trait::async_trait;
+pub use ::serde;
+pub use ::tracing;
+
 pub use error::{Result, AnyhowContext};
+pub use inflector::{Inflector, Inflections, init_inflections};
